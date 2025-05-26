efa4b47b9ac453adb9e9a4db24497db8e4582394
park hyung jun <55447614+wamma@users.noreply.github.com>
2025-05-26 17:01:01 +0900

    flutter 앱 뼈대 완성

--- baseball/lib/main.dart
+++ baseball/lib/main.dart
@@ -1,122 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+const Map<String, String> kboTeams = {
+  'LG 트윈스': 'assets/logos/lg.png',
+  'KT 위즈': 'assets/logos/kt.png',
+  'SSG 랜더스': 'assets/logos/ssg.png',
+  'NC 다이노스': 'assets/logos/nc.png',
+  'KIA 타이거즈': 'assets/logos/kia.png',
+  '두산 베어스': 'assets/logos/bears.png',
+  '롯데 자이언츠': 'assets/logos/lotte.jpg',
+  '삼성 라이온즈': 'assets/logos/samsung.png',
+  '한화 이글스': 'assets/logos/hanwha.jpg',
+  '키움 히어로즈': 'assets/logos/kia.png',
+};
 
-  // This widget is the root of your application.
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'My KBO Team',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: TeamSelectionPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class TeamSelectionPage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _TeamSelectionPageState createState() => _TeamSelectionPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _TeamSelectionPageState extends State<TeamSelectionPage> {
+  String? selectedTeam;
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    loadTeam();
+  }
+
+  Future<void> loadTeam() async {
+    final prefs = await SharedPreferences.getInstance();
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      selectedTeam = prefs.getString('myTeam');
+    });
+  }
+
+  Future<void> saveTeam(String team) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('myTeam', team);
+    setState(() {
+      selectedTeam = team;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('마이팀 설정'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      body: ListView.builder(
+        itemCount: kboTeams.length,
+        itemBuilder: (context, index) {
+          final team = kboTeams.keys.elementAt(index);
+          final isSelected = team == selectedTeam;
+          return ListTile(
+            title: Text(''),
+            leading: Image.asset(kboTeams[team]!),
+            trailing: isSelected ? Icon(Icons.check, color: Colors.green) : null,
+            onTap: () => saveTeam(team),
+          );
+        },
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
